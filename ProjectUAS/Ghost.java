b900570c61d1bc08a735ac369db4e3ab88e0795f
MUHAMMADALIEFRAMADHAN <148423509+MUHAMMADALIEFRAMADHAN@users.noreply.github.com>
2023-12-22 19:15:08 +0800

    Add files via upload

---
+++ ProjectUAS/Ghost.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+public class Ghost extends Actor
+{
+    // instance or state variables
+    public int speed = 2;
+    public String direction = "down";
+        
+    // Method called whenever act or run is pressed.
+    public void act() 
+    {
+       setLocation(getX(), getY() + speed);
+       atWall();
+    }
+    // Changes the direction of the bug.
+    public void changeDirection()
+    {
+       if (direction.equals("down")){
+           direction = "up";
+       }
+    }
+    // If the ghost is at the wall, have it change direction.
+    public void atWall()
+    {
+        Actor wall = getOneIntersectingObject(Wall.class);
+        if (wall != null){
+            speed = -speed;
+            changeDirection();
+        }
+    }
+}
